ab3593a8b45265e7129060660d48df1f6e841da4
AndreyKapus <kapustnikov@ukr.net>
2023-03-11 13:44:25 +0200

    randomize color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function onChangeColor() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startButtonEl.classList.add("disabled")
+  }, 1000);
+};
+
+function onStopChangeColor() {
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButtonEl.addEventListener('click', onChangeColor);
+stopButtonEl.addEventListener('click', onStopChangeColor)
