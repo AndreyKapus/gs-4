b984f48ed5ee399212229f6c3659e6f474f10e60
AndreyKapus <kapustnikov@ukr.net>
2023-03-12 13:56:12 +0200

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -27,11 +27,51 @@ const options = {
 };
 
 function handlestartTimer() {
+  const intervalId = setInterval(() => {
+    const selectedDate = flatPickr.selectedDates[0];
+    const currentDate = Date.now();
 
+    const deltaTime = selectedDate - currentDate;
+    const timer = convertMs(deltaTime);
+    updateClockFace(timer);
+
+    if(deltaTime === 0) {
+      clearInterval(intervalId)
+    }
+
+  }, 1000)
 }
 
-const onStartButtonClick = startBtnEl.addEventListener('click', handlestartTimer)
+function updateClockFace({days, hours, minutes, seconds}) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
 
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0')
+};
+
+startBtnEl.addEventListener('click', handlestartTimer)
 
 const flatPickr = flatpickr(inputEl, options);
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
