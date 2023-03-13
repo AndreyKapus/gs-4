ff530c2e446d1d496dd83e205fac856ae17e22bf
AndreyKapus <kapustnikov@ukr.net>
2023-03-13 13:32:33 +0200

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+const formEl = document.querySelector('.form');
+const inputDelayEl = document.querySelector('input[name=delay]');
+const inputStepEl = document.querySelector('input[name=step]');
+const inputAmountEl = document.querySelector('input[name=amount]');
+const submitBtnEl = document.querySelector('button')
+console.log(inputDelayEl)
+
+
 function createPromise(position, delay) {
+  const promise = new Promise;
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return promise;
   } else {
-    // Reject
+    return console.log('error')
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+}
+
+formEl.addEventListener('submit', onFormSubmit)
